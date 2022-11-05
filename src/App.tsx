c6c040fc00b4c74b0c395f44f27b094673acf682
Pablo Hansen <vicky@Pablos-MacBook-Air.local>
2022-11-04 21:23:07 -0700

    Table first prototype

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 import "./App.css";
-import { Navbar } from "./components/Navbar";
+import { Navbar } from "./components/navbar";
+
+import Table from "./components/Table";
 
 function App() {
   return (
-    <div className="App h-screen" style={{ background: "#10172A" }}>
-      <Navbar />
+    <div>
+      <div className="App h-screen" style={{ background: "#10172A" }}>
+        <Navbar />
+      </div>
+      <div className="App">
+        <Table />
+      </div>
     </div>
   );
 }
---
+++ src/components/Table.tsx
@@ -0,0 +1,324 @@
+
+
+const Table = () => {
+    return (
+        <div>
+
+            <div className="divider"></div>
+            <div className="overflow-x-auto">
+                <p className="text-4xl">Top Vaults</p>
+                <table className="table w-full">
+
+
+                    <thead>
+                        <tr>
+                            <th>Collection</th>
+                            <th>APY</th>
+                            <th>Rewards</th>
+                            <th>Rewards Expiry</th>
+                            <th>TVL</th>
+                            <th>Status</th>
+                            <th>Service</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th className="active">
+                                <div className="avatar-group -space-x-6">
+                                    <div className="avatar">
+                                        <div className="w-12">
+                                            <img src="https://placeimg.com/192/192/people" />
+                                        </div>
+                                    </div>
+                                    <div className="avatar">
+                                        <div className="w-12">
+                                            <img src="https://placeimg.com/192/192/people" />
+                                        </div>
+                                    </div>
+                                    <div className="avatar">
+                                        <div className="w-12">
+                                            <img src="https://placeimg.com/192/192/people" />
+                                        </div>
+                                    </div>
+                                    <div className="avatar">
+                                        <div className="w-12">
+                                            <img src="https://placeimg.com/192/192/people" />
+                                        </div>
+                                    </div>
+                                </div>
+
+
+                            </th>
+                            <td>17.33%</td>
+                            <td>
+                                <div className="avatar-group -space-x-6">
+                                    <div className="avatar">
+                                        <div className="w-8">
+                                            <img src="https://placeimg.com/192/192/people" />
+                                        </div>
+                                    </div>
+                                    <div className="avatar">
+                                        <div className="w-8">
+                                            <img src="https://placeimg.com/192/192/people" />
+                                        </div>
+                                    </div>
+                                </div>
+                            </td>
+
+
+                            <td>November 14 2022</td>
+                            <td>$38,820</td>
+                            <td>
+                                Live
+                            </td>
+                            <td><b>Sudoswap</b></td>
+                        </tr>
+                        <tr>
+                            <th>
+                                <div className="avatar-group -space-x-6">
+                                    <div className="avatar">
+                                        <div className="w-12">
+                                            <img src="https://placeimg.com/192/192/people" />
+                                        </div>
+                                    </div>
+                                    <div className="avatar">
+                                        <div className="w-12">
+                                            <img src="https://placeimg.com/192/192/people" />
+                                        </div>
+                                    </div>
+                                    <div className="avatar">
+                                        <div className="w-12">
+                                            <img src="https://placeimg.com/192/192/people" />
+                                        </div>
+                                    </div>
+                                    <div className="avatar">
+                                        <div className="w-12">
+                                            <img src="https://placeimg.com/192/192/people" />
+                                        </div>
+                                    </div>
+                                </div>
+
+
+                            </th>
+                            <td>17.33%</td>
+                            <td>
+                                <div className="avatar-group -space-x-6">
+                                    <div className="avatar">
+                                        <div className="w-8">
+                                            <img src="https://placeimg.com/192/192/people" />
+                                        </div>
+                                    </div>
+                                    <div className="avatar">
+                                        <div className="w-8">
+                                            <img src="https://placeimg.com/192/192/people" />
+                                        </div>
+                                    </div>
+                                </div>
+                            </td>
+
+
+                            <td>November 14 2022</td>
+                            <td>$38,820</td>
+                            <td>
+                                Live
+                            </td>
+                            <td><b>Sudoswap</b></td>
+                        </tr>
+                        <tr>
+                            <th>
+                                <div className="avatar-group -space-x-6">
+                                    <div className="avatar">
+                                        <div className="w-12">
+                                            <img src="https://placeimg.com/192/192/people" />
+                                        </div>
+                                    </div>
+                                    <div className="avatar">
+                                        <div className="w-12">
+                                            <img src="https://placeimg.com/192/192/people" />
+                                        </div>
+                                    </div>
+                                    <div className="avatar">
+                                        <div className="w-12">
+                                            <img src="https://placeimg.com/192/192/people" />
+                                        </div>
+                                    </div>
+                                    <div className="avatar">
+                                        <div className="w-12">
+                                            <img src="https://placeimg.com/192/192/people" />
+                                        </div>
+                                    </div>
+                                </div>
+
+
+                            </th>
+                            <td>17.33%</td>
+                            <td>
+                                <div className="avatar-group -space-x-6">
+                                    <div className="avatar">
+                                        <div className="w-8">
+                                            <img src="https://placeimg.com/192/192/people" />
+                                        </div>
+                                    </div>
+                                    <div className="avatar">
+                                        <div className="w-8">
+                                            <img src="https://placeimg.com/192/192/people" />
+                                        </div>
+                                    </div>
+                                </div>
+                            </td>
+
+
+                            <td>November 14 2022</td>
+                            <td>$38,820</td>
+                            <td>
+                                Live
+                            </td>
+                            <td><b>Sudoswap</b></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div >
+
+            <button className="btn btn-outline btn-info rounded">View More</button>
+
+            <div className="divider"></div>
+            <p className="text-4xl">Popular Collections</p>
+
+            <div className="overflow-x-auto">
+                <table className="table w-full">
+
+                    <thead>
+                        <tr>
+                            <th>Collection</th>
+                            <th>Volume</th>
+                            <th>Sales</th>
+                            <th>Avg. Price</th>
+                            <th>Owners</th>
+                            <th>Items</th>
+                            <th>Service</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>
+                                <div className="avatar ">
+                                    <div className="w-12 rounded-full">
+                                        <img src="https://placeimg.com/192/192/people" />
+                                    </div>
+                                </div>
+                                Azuki
+
+                            </td>
+                            <td>
+                                <div className="avatar ">
+                                    <div className="w-7 rounded-full">
+                                        <img src="https://placeimg.com/192/192/people" />
+                                    </div>
+                                </div>
+                                242.36
+                            </td>
+                            <td>
+                                16
+                            </td>
+
+
+                            <td>
+                                <div className="avatar ">
+                                    <div className="w-7 rounded-full">
+                                        <img src="https://placeimg.com/192/192/people" />
+                                    </div>
+                                </div>
+                                242.36
+                            </td>
+                            <td>5,041</td>
+                            <td>
+                                1,000
+                            </td>
+                            <td><b>Sudoswap</b></td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <div className="avatar ">
+                                    <div className="w-12 rounded-full">
+                                        <img src="https://placeimg.com/192/192/people" />
+                                    </div>
+                                </div>
+                                Azuki
+
+                            </td>
+                            <td>
+                                <div className="avatar ">
+                                    <div className="w-7 rounded-full">
+                                        <img src="https://placeimg.com/192/192/people" />
+                                    </div>
+                                </div>
+                                242.36
+                            </td>
+                            <td>
+                                16
+                            </td>
+
+
+                            <td>
+                                <div className="avatar ">
+                                    <div className="w-7 rounded-full">
+                                        <img src="https://placeimg.com/192/192/people" />
+                                    </div>
+                                </div>
+                                242.36
+                            </td>
+                            <td>5,041</td>
+                            <td>
+                                1,000
+                            </td>
+                            <td><b>Sudoswap</b></td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <div className="avatar ">
+                                    <div className="w-12 rounded-full">
+                                        <img src="https://placeimg.com/192/192/people" />
+                                    </div>
+                                </div>
+                                Azuki
+
+                            </td>
+                            <td>
+                                <div className="avatar ">
+                                    <div className="w-7 rounded-full">
+                                        <img src="https://placeimg.com/192/192/people" />
+                                    </div>
+                                </div>
+                                242.36
+                            </td>
+                            <td>
+                                16
+                            </td>
+
+
+                            <td>
+                                <div className="avatar ">
+                                    <div className="w-7 rounded-full">
+                                        <img src="https://placeimg.com/192/192/people" />
+                                    </div>
+                                </div>
+                                242.36
+                            </td>
+                            <td>5,041</td>
+                            <td>
+                                1,000
+                            </td>
+                            <td><b>Sudoswap</b></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div >
+        </div>
+
+
+    );
+}
+
+
+
+export default Table;
