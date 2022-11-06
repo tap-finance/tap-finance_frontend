94cf1a78e4e5dbdb615a0f3e3e4ad56394c2ca07
dario-macedo-nftStudios <dario.macedo@nftstudios.io>
2022-11-06 00:00:22 -0700

    liquidity pool table nfts with no nfts message

--- src/components/Home/NftLiquidityPool.tsx
+++ src/components/Home/NftLiquidityPool.tsx
@@ -6,6 +6,7 @@ import { useNetwork } from "wagmi";
 import { InfinitySpin } from "react-loader-spinner";
 import { shortenWalletAddress } from "../../utils/shortenWallet";
 import axios from "axios";
+import { FaEthereum } from "react-icons/fa";
 
 const NftLiquidityPool = () => {
   const [dataDune, setDataDune] = useState([] as any);
@@ -52,6 +53,7 @@ const NftLiquidityPool = () => {
 
       // setDataDune(apiResponse.json(products));
       setDataDune(products?.result.rows);
+
       setIsLoading(false);
     });
 
@@ -67,78 +69,94 @@ const NftLiquidityPool = () => {
       <div className="pb-7">
         <p className="text-3xl">Liquidity pool for your NFT</p>
       </div>
+      {!nfts ? (
+        <h1>
+          Sorry you can't see the section because you don't have no NFTs in this
+          wallet
+        </h1>
+      ) : (
+        nfts?.ownedNfts.map((nft: any) => (
+          <div key={nft?.tokenId} className="flex pb-10">
+            <div className="">
+              {nft?.media[0].gateway.includes(".mp4") ? (
+                <iframe
+                  title="nftvid"
+                  className=" aspect-video aspect-square  mr-7 rounded-xl border-2 mb-1"
+                  src={nft?.media[0].gateway}
+                  style={{ height: "100px" }}
+                ></iframe>
+              ) : (
+                <img
+                  className=" mr-7 rounded-xl border-2 mb-1"
+                  src={nft?.media[0].gateway}
+                  alt=""
+                  style={{ height: "100px" }}
+                />
+              )}
 
-      {nfts?.ownedNfts.map((nft: any) => (
-        <div key={nft?.tokenId} className="flex pb-10">
-          <div className="">
-            {nft?.media[0].gateway.includes(".mp4") ? (
-              <iframe
-                title="nftvid"
-                className=" aspect-video aspect-square  mr-7 rounded-xl border-2 mb-1"
-                src={nft?.media[0].gateway}
-                style={{ height: "100px" }}
-              ></iframe>
-            ) : (
-              <img
-                className=" mr-7 rounded-xl border-2 mb-1"
-                src={nft?.media[0].gateway}
-                alt=""
-                style={{ height: "100px" }}
-              />
-            )}
+              <span className="badge badge-m badge-outline w-24">
+                {nft?.title.substring(0, nft?.title.indexOf(" ")) + "..."}
+              </span>
+              <p className="text-zinc-400" style={{ fontSize: "12px" }}>
+                {nft?.tokenType}
+              </p>
+              <></>
+              <p className="text-zinc-400" style={{ fontSize: "12px" }}>
+                {shortenWalletAddress(nft?.contract.address)}
+              </p>
+            </div>
 
-            <span className="badge badge-m badge-outline w-24">
-              {nft?.title.substring(0, nft?.title.indexOf(" ")) + "..."}
-            </span>
-            <p className="text-zinc-400" style={{ fontSize: "12px" }}>
-              {nft?.tokenType}
-            </p>
-            <></>
-            <p className="text-zinc-400" style={{ fontSize: "12px" }}>
-              {shortenWalletAddress(nft?.contract.address)}
-            </p>
+            <div className="overflow-x-auto">
+              <table className="table">
+                <thead>
+                  <tr>
+                    <th>APY</th>
+                    <th>24h Volume</th>
+                    <th>TVL</th>
+                    <th>Status</th>
+                    <th>Service</th>
+                  </tr>
+                </thead>
+
+                <tbody>
+                  {!dataDune ? (
+                    <InfinitySpin width="200" color="#4fa94d" />
+                  ) : (
+                    dataDune?.map((d: any) => (
+                      <tr className="hover">
+                        <td>{(d?.tvl * 0.2).toFixed(2)}%</td>
+
+                        <td>${(d?.volume_24hr).toFixed(2)}</td>
+
+                        <td>
+                          <span className="flex ">
+                            <FaEthereum
+                              style={{ paddingRight: "5px", marginTop: "3px" }}
+                            />
+
+                            {d?.tvl.toFixed(2)}
+                          </span>
+                        </td>
+
+                        <td className="flex">
+                          <img src={greenCircle} alt="" className="w-2 mr-1" />
+                          <p>Live</p>
+                        </td>
+                        <td>
+                          <b>Sudoswap</b>
+                        </td>
+                      </tr>
+                    ))
+                  )}
+                </tbody>
+              </table>
+              <button className="btn success mt-2 w-full bg-sky-700 text-stone-50">
+                + Create a new pool on tapp.finance
+              </button>
+            </div>
           </div>
-
-          <div className="overflow-x-auto">
-            <table className="table">
-              <thead>
-                <tr>
-                  <th>APY</th>
-                  <th>Rewards Expiry</th>
-                  <th>TVL</th>
-                  <th>Status</th>
-                  <th>Service</th>
-                </tr>
-              </thead>
-
-              <tbody>
-                {!dataDune ? (
-                  <InfinitySpin width="200" color="#4fa94d" />
-                ) : (
-                  dataDune?.map((d: any) => (
-                    <tr className="hover">
-                      <td>{(d?.tvl * 0.2).toFixed(2)}%</td>
-
-                      <td>November 14 2022</td>
-                      <td>${d?.tvl.toFixed(2)}</td>
-                      <td className="flex">
-                        <img src={greenCircle} alt="" className="w-2 mr-1" />
-                        <p>Live</p>
-                      </td>
-                      <td>
-                        <b>Sudoswap</b>
-                      </td>
-                    </tr>
-                  ))
-                )}
-              </tbody>
-            </table>
-            <button className="btn success mt-2 w-full bg-sky-700 text-stone-50">
-              + Create a new pool on tapp.finance
-            </button>
-          </div>
-        </div>
-      ))}
+        ))
+      )}
     </div>
   );
 };
